b5af17baf443f52535b99a634aaa29082b60c325
JAGHAN T <108980892+jaghant@users.noreply.github.com>
2024-03-17 15:18:35 +0530

    Update Running.py

--- Running.py
+++ Running.py
@@ -2,25 +2,30 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 from streamlit_option_menu import option_menu
+from pathlib import Path
+
 
 # Display Title and Description
 st.title("🏃‍♂️Running and Cycling Report🚴‍♂️")
-st.markdown("Enter the details of the new record below.")
+# st.markdown("Enter the details of the new record below.")
 
 selected = option_menu(
         menu_title    = None,
-        options       = ["Running and Cycling Data Entry", "Running and Cycling Reports"],
+        options       = ["Running and Cycling Data Entry", "Running and Cycling Reports", "Dashboard"],
         icons         = ["diagram-3-fill", "bar-chart-fill"],
         menu_icon     = "cast",
         default_index = 0,
         orientation   = "horizontal"
 )
+conn = st.connection("gsheets", type=GSheetsConnection)
+existing_data = conn.read(wordsheet="Running", usecols=list(range(6)), ttl=5)
+existing_data = existing_data.dropna(how="all")
 
 if selected == "Running and Cycling Data Entry":
         st.subheader(f"{selected}")
         
         # Establishing a Google Sheets connection
-        conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+        conn = st.connection("gsheets", type=GSheetsConnection)
 
         # Fetch existing running data
         existing_data = conn.read(wordsheet="Running", usecols=list(range(6)), ttl=5)
@@ -81,9 +86,27 @@ if selected == "Running and Cycling Data Entry":
 if selected == "Running and Cycling Reports":
         st.subheader(f"{selected}")
         
-        conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+        conn = st.connection("gsheets", type=GSheetsConnection)
         # Fetch existing running data
         existing_data = conn.read(worksheet="Running", usecols=list(range(6)), ttl=5)
         existing_data = existing_data.dropna(how="all")
         
         st.dataframe(existing_data)    
+
+if selected == "Dashboard":
+        st.subheader(f"{selected}")
+        
+        with open('style.css') as f:
+            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+        
+        Left_column, Right_column = st.columns(2)
+            
+        with Left_column:
+            Total_distance = int(existing_data["Distance KM"].sum())
+            st.metric(label="Total Distance", value=(f"{Total_distance} KM"))       
+        with Right_column:    
+            Total_calories = int(existing_data["Calories"].sum())
+            d = st.metric(label="Total Calories", value=(f"{Total_calories}"))
+        
+    
+        
